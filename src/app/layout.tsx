65dd38baaeca73ad3778e7c9c381be7a976844f6
Jonathan Boss <jboss@Jonathans-MBP.wifi.gsu.edu>
2024-02-20 14:17:00 -0500

    added bootstrap icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
 import '@/app/ui/globals.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import React from "react";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
--- src/app/ui/dashboard/(media_player)/mediacenter.tsx
+++ src/app/ui/dashboard/(media_player)/mediacenter.tsx
@@ -5,9 +5,10 @@ export default function MediaCenter() {
        <div className="flex flex-row min-h-28 bg-black items-center justify-center">
            {/*<p className="text-white">Media Center</p>*/}
            <div className="flex flex-row text-white space-x-3">
+               <i className="bi bi-shuffle py-5"></i>
                <BackwardIcon className="w-8"/>
-               <PlayCircleIcon className="w-16" />
-               <ForwardIcon className="w-8" />
+               <PlayCircleIcon className="w-16"/>
+               <ForwardIcon className="w-8"/>
                <ArrowPathRoundedSquareIcon className="w-5"/>
            </div>
        </div>
